ad64d6d11d161fc91fd1bb928599bc12286adcf4
dungpt3003 <dungpt3003@gmail.com>
2016-04-24 00:14:18 +0700

    Authorize user

--- Modem.py
+++ Modem.py
@@ -63,13 +63,12 @@ class Modem():
             elif "REC UNREAD" in out:
             #else:
                 body = self.modem.readline()
-                while(not process.process.authorized):
+                if (not process.process.authorized):
                     try:
                         process.process.authorize(body.split(' ')[0], body.split(' ')[1])
                     except:
                         response = "Please send your username and password, split by a whitespace"
-
-                if (process.process.authorized):
+                else:
                     response = process.execute(body.splitlines()[0])
 
                 time.sleep(1)
